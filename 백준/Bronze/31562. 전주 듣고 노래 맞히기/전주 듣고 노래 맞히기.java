f7579412f3c3c4fd70e368de8a681d571fc240ed
aoooec <112041983+aoooec@users.noreply.github.com>
2025-01-23 21:23:52 +0900

    [Bronze I] Title: 전주 듣고 노래 맞히기, Time: 196 ms, Memory: 16984 KB -BaekjoonHub

---
+++ 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().trim().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+        String[] songInput = new String[N];
+        HashMap<String, String> songs = new HashMap<>();
+
+        for(int i = 0; i < N; i++) {
+            songInput[i] = br.readLine();
+            String[] song = songInput[i].split(" ");
+            String key = song[2] + song[3] + song[4];
+
+            if (!songs.containsKey(key)) {
+                songs.put(key, song[1]);
+            } else {
+                songs.put(key, "?");
+            }
+        }
+
+        for(int i = 0; i < M; i++) {
+            String music = br.readLine().replaceAll(" ", "");
+
+            System.out.println(songs.getOrDefault(music, "!"));
+        }
+
+    }
+
+}
