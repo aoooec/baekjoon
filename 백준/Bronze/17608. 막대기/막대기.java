234574430c6670953a28d5454b87aca86a4f7a4c
aoooec <112041983+aoooec@users.noreply.github.com>
2025-02-04 21:25:46 +0900

    [Bronze II] Title: 막대기, Time: 172 ms, Memory: 21572 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.java
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -1,25 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int count = 0;
-        int max = 0;
-        Stack<Integer> stack = new Stack<Integer>();
+        int[] nums = new int[N];
 
         for(int i = 0; i < N; i++) {
-            stack.add(Integer.parseInt(br.readLine()));
+            nums[i] = Integer.parseInt(br.readLine());
         }
 
-        for(int i = 0; i < N; i++) {
-            int num = stack.pop();
-            if(max < num) {
+        int count = 1;
+        int max = nums[N-1];
+
+        for(int i = N - 2; i >= 0; i--) {
+            if(max < nums[i]) {
                 count++;
-                max = num;
+                max = nums[i];
             }
         }
 
