9fefb4b52ba0ee83a87f93b2e545bd7cd5a381a3
aoooec <112041983+aoooec@users.noreply.github.com>
2025-02-05 22:53:21 +0900

    [Silver IV] Title: 큐, Time: 136 ms, Memory: 18616 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> q = new ArrayDeque<>();
+
+        for(int i = 0; i < N; i++) {
+            String command = br.readLine();
+
+            switch(command) {
+                case "pop":
+                    bw.write((q.isEmpty() ? -1 : q.pop()) + "\n");
+                    break;
+                case "size":
+                    bw.write(q.size() + "\n");
+                    break;
+                case "empty":
+                    bw.write((q.isEmpty() ? 1 : 0) + "\n");
+                    break;
+                case "front":
+                    bw.write((q.isEmpty() ? -1 : q.peek()) + "\n");
+                    break;
+                case "back":
+                    bw.write((q.isEmpty() ? -1 : q.peekLast()) + "\n");
+                    break;
+                default:
+                    int num = Integer.parseInt(command.split(" ")[1]);
+                    q.add(num);
+                    break;
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
