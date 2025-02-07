15acde83985a8eac6a6d2b9df9a6c48a4ea7361a
aoooec <112041983+aoooec@users.noreply.github.com>
2025-02-08 01:10:33 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 68 ms, Memory: 11884 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = i + 1;
+        }
+
+        for(int round = 0; round < M; round++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken()) - 1;
+            int j = Integer.parseInt(st.nextToken()) - 1;
+
+            while (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        for(int num: arr) {
+            bw.write(num + " ");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
