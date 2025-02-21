38ce39062e9a3f3d050f13e7397ede6b0a51e011
aoooec <112041983+aoooec@users.noreply.github.com>
2025-02-21 23:47:20 +0900

    [Bronze V] Title: 새싹, Time: 60 ms, Memory: 11372 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7").append("\n").append("r`-_   ,'  ,/").append("\n").append(" \\. \". L_r'").append("\n").append("   `~\\/").append("\n").append("      |").append("\n").append("      |");
+        System.out.print(sb.toString());
+    }
+}
