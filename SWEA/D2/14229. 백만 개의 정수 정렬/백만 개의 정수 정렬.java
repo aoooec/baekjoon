a7308a0819cf55c3e3e1e179155c57b5a36f273f
aoooec <112041983+aoooec@users.noreply.github.com>
2025-03-06 13:59:09 +0900

    [D2] Title: 백만 개의 정수 정렬, Time: 354 ms, Memory: 116,016 KB -BaekjoonHub

--- SWEA/D2/14229. 백만 개의 정수 정렬/백만 개의 정수 정렬.java
+++ SWEA/D2/14229. 백만 개의 정수 정렬/백만 개의 정수 정렬.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 
 public class Solution {
 	static int N = 1000000;
-	static int[] arr = new int[N], tmp = new int[N];
+	static int[] arr = new int[N];
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,42 +12,34 @@ public class Solution {
 		for(int i = 0; i < 1000000; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		} // 배열 채우기
-		mergeSort(0, N-1);
+		quickSort(0, N-1);
 		System.out.print(arr[500000]);
 	} // end of main
 	
-	static void mergeSort(int start, int end) {
-		if(start >= end) return;
-		int mid = (start + end) / 2;
-		mergeSort(start, mid);
-		mergeSort(mid+1, end);
-		merge(start, mid, end);
+	static void quickSort(int start, int end) {
+		if(start < end) {
+			int pivot = partition(start, end);
+			quickSort(start, pivot-1);
+			quickSort(pivot+1, end);
+		}
 	}
 	
-	static void merge(int start, int mid, int end) {
-		int L = start;
-		int R = mid + 1;
-		int idx = start;
+	static int partition(int start, int end) {
+		int pivot = arr[end]; // 로무토 -> 마지막 요소가 피봇 
+		int i = start - 1; // pivot보다 작은 값을 가지는 부분의 경계 
 		
-		while(L <= mid && R <= end) {
-			if(arr[L] <= arr[R]) {
-				tmp[idx++] = arr[L++];
-			} else {
-				tmp[idx++] = arr[R++];
+		for(int j = start; j < end; j++) { 
+			if(arr[j] <= pivot) { // 값이 pivot보다 작거나 같으면 
+				i++; // 경계 +1
+				int tmp = arr[i]; 
+				arr[i] = arr[j]; // 경계 안에 해당 값을 넣음 
+				arr[j] = tmp; // pivot보다 큰 값은 경계 밖으로 넘김 
 			}
-		} 
-		
-		if(L <= mid) {
-			for(int i = L; i <= mid; i++) {
-				tmp[idx++] = arr[i]; 
-			}
-		} else {
-			for(int i = R; i <= end; i++) {
-				tmp[idx++] = arr[i]; 
-			}
-		} // 남은 수가 있으면 입력 
-		for(int i = start; i <= end; i++) {
-			arr[i] = tmp[i];
 		}
+		int tmp = arr[i+1]; // pivot이 있어야 할 자리 i+1
+		arr[i+1] = arr[end];
+		arr[end] = tmp;
+		
+		return i+1;
 	}
 } // end of class
