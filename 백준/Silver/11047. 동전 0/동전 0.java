1c59d05491f0dec1b02bd6e8393e72f9ae06dc45
aoooec <112041983+aoooec@users.noreply.github.com>
2025-03-18 17:35:22 +0900

    [Silver IV] Title: 동전 0, Time: 68 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -2,43 +2,23 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-	static int min;
-	private static int[] coin;
 	public static void main(String[] args) throws Exception {
 		// 1703-1719
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] input = br.readLine().split(" ");
 		int N = Integer.parseInt(input[0]); // 1~10, 동전 종류
 		int K = Integer.parseInt(input[1]); // 1~100000000, 만들 합 
-		coin = new int[N]; 
+		int[] coin = new int[N]; 
 		for(int i = 0; i < N; i++) {
 			coin[i] = Integer.parseInt(br.readLine());
 		}
-		min = K;
-		comb(0, N-1, K);
-		System.out.print(min);
-	}
-	
-//	static void comb(int cnt, int idx, int rest) {
-//		if(cnt >= min) return;
-//		if(rest == 0) {
-//			min = Math.min(min, cnt);
-//			return;
-//		}
-//		if(idx < 0) return;
-//		
-//		if(rest - coin[idx] >= 0) comb(cnt+1, idx, rest-coin[idx]);
-//		else comb(cnt, idx-1, rest);
-//	}
-	static void comb(int cnt, int idx, int rest) {
-		if(cnt >= min) return;
-		if(rest == 0) {
-			min = Math.min(min, cnt);
-			return;
+		int cnt = 0;
+		for(int i = N-1; i >= 0; i--) {
+			if(K - coin[i] >= 0) {
+				cnt += K/coin[i];
+				K = K%coin[i];
+			}
 		}
-		if(idx < 0) return;
-		
-		if(rest - coin[idx] >= 0) comb(cnt+(rest/coin[idx]), idx-1, rest%coin[idx]);
-		else comb(cnt, idx-1, rest);
+  		System.out.print(cnt);
 	}
 }
