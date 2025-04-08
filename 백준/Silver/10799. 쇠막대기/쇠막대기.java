52a7249805977a40a9fd133b320f10f410ef4174
aoooec <112041983+aoooec@users.noreply.github.com>
2025-04-08 12:04:38 +0900

    [Silver II] Title: 쇠막대기, Time: 84 ms, Memory: 13952 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,12 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input = br.readLine();
-		Stack<Character> stack = new Stack<>();
 		int count = 0; 
 		int layer = 0;
 		boolean prevRight = false;
@@ -14,14 +12,12 @@ public class Main {
 			char c = input.charAt(i);
 			switch(c) {
 			case '(':
-				stack.push(c);
 				layer++;
 				prevRight = false;
 				break;
 			case ')':
 				layer--;
 				if(prevRight) {
-					stack.pop();
 					count++;
 				} else {
 					prevRight = true;
