0960db8e29e3a9d5b1d6a15ebf4e8c04d8f9cd09
aoooec <112041983+aoooec@users.noreply.github.com>
2025-04-20 23:03:14 +0900

    [Silver II] Title: 리그 오브 레전설 (Small), Time: 68 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int mod = 1000000007;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken()); // 1~10000
+        int M = Integer.parseInt(st.nextToken()); // 2~100
+        int[] dp = new int[N+1];
+        dp[0] = 1;
+        for(int i = 1; i <= N; i++) {
+            dp[i] = dp[i-1];
+            if(i >= M) dp[i] = dp[i] + dp[i-M];
+            dp[i] %= mod;
+        }
+        System.out.print(dp[N]);
+    }
+}
