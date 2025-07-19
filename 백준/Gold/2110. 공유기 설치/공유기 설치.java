4243f23576d77643f3b173bc934d07f81ec71162
aoooec <112041983+aoooec@users.noreply.github.com>
2025-07-19 23:55:49 +0900

    [Gold IV] Title: 공유기 설치, Time: 236 ms, Memory: 28488 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());        // 2~200000
+        int C = Integer.parseInt(st.nextToken());        // 2 <= C <= N
+        int[] houses = new int[N];
+        for(int i = 0; i < N; i++) {
+            houses[i] = Integer.parseInt(br.readLine()); // 0~1000000000
+        }
+        Arrays.sort(houses);                             // 이분탐색을 위한 정렬
+        int max = houses[N - 1] - houses[0];             // 집 사이의 최장 거리
+        int min = 1;                                     // 최소 거리
+        int ans = 0;
+        while(min <= max) {
+            int mid = (min + max) / 2;
+            int cnt = 1;                                 // 공유기 설치 개수
+            int prev = houses[0];                        // 이전 공유기 설치 위치
+            for(int i = 1; i < N && cnt <= C; i++) {
+                if(houses[i] - prev >= mid) {
+                    cnt++;
+                    prev = houses[i];
+                }
+            }
+            if(cnt >= C) {
+                min = mid + 1;
+                ans = mid;
+            } else max = mid - 1;
+        }
+        System.out.print(ans);
+    }
+}
