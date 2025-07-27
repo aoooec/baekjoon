0028b6af6d7acef697637678d30bd5fa4c863634
aoooec <112041983+aoooec@users.noreply.github.com>
2025-07-27 23:34:42 +0900

    [Bronze V] Title: We love kriii, Time: 68 ms, Memory: 11248 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        String s = "강한친구 대한육군";
+        sb.append(s).append("\n").append(s);
+        System.out.print(sb.toString());
+    }
+}
