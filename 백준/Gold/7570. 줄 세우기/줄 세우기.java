a69633276bab02b32c749836145135266f47a902
aoooec <112041983+aoooec@users.noreply.github.com>
2026-02-09 20:28:37 +0900

    [Gold II] Title: 줄 세우기, Time: 372 ms, Memory: 120296 KB -BaekjoonHub

---
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] pos = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            pos[num] = i;
+        }
+
+        int maxL = 1;
+        int curr = 1;
+
+        for (int i = 2; i <= N; i++) {
+            if (pos[i] > pos[i - 1]) curr++;
+            else curr = 1;
+
+            if (curr > maxL) maxL = curr;
+        }
+
+        System.out.print(N - maxL);
+    }
+}
