4e57d844aff6d10a49c873ab13f891dc3a228c86
aoooec <112041983+aoooec@users.noreply.github.com>
2026-03-24 22:22:25 +0900

    [Silver III] Title: 01타일, Time: 92 ms, Memory: 15432 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final int MOD = 15746;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if (N == 1) {
+            System.out.print(1);
+            return;
+        }
+        if (N == 2) {
+            System.out.print(2);
+            return;
+        }
+
+        int[] dp = new int[N + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        System.out.print(dp[N]);
+    }
+}
